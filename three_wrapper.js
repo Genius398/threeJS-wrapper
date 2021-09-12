a50d7be4dc0d1b10fb44ebbec5586f236a9e2d5b
Adatta1276 <dattaayush08@gmail.com>
2021-09-12 15:45:15 +0530

    Add files via upload

---
+++ three_wrapper.js
@@ -0,0 +1,16 @@
+class ThreeWrapper_MeshBasicMaterial {
+    constructor() {
+       this.allProps = {
+        alphaMap = null,
+        aoMap = null,
+        aoMapIntensity = null,
+        bumpMap = null,
+        
+       }
+            
+            
+        
+        
+        
+    }
+}
